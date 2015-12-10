804a58bcd50ce97ba522081d2411e245a312ad0e
Andres <astadel5679@gmail.com>
2015-12-10 22:24:04 +0100

    Fixed the maze.reset() method
    Now the main class "Console.java" can be called for the zoo animals
    since the reset() function manages to function appropriately

--- src/ch/epfl/maze/physical/Maze.java
+++ src/ch/epfl/maze/physical/Maze.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ch.epfl.maze.util.Vector2D;
+import ch.epfl.maze.physical.Animal;
 
 /**
  * Maze in which an animal starts from a starting point and must find the exit.
@@ -14,9 +15,12 @@ import ch.epfl.maze.util.Vector2D;
 
 public final class Maze extends World {
 	
-	private List<Animal> endAnimals = new ArrayList<Animal>();
+	//We create two lists, the first of which will contain all the animals that are currently moving in the maze at any given moment
+	//The second basically stores the same animal, but this will never change even as the animal moves. This serves for the reset() method which requires an exact copy of the 
+	//animal initially added into the maze, effectively preserving all of it's original attributes
 	private List<Animal> navAnimals = new ArrayList<Animal>();
-
+	private List<Animal> endAnimals = new ArrayList<Animal>();
+	
 	/**
 	 * Constructs a Maze with a labyrinth structure.
 	 * 
@@ -25,21 +29,16 @@ public final class Maze extends World {
 	 */
 
 	public Maze(int[][] labyrinth) {
-		super(labyrinth); //What is going on here?
-		// TODO
+		super(labyrinth); 
 	}
 
 	@Override
 	public boolean isSolved() {
-		//navAnimals = getAnimals();
 		return navAnimals.isEmpty();
-		// TODO
 	}
 
 	@Override
-	public List<Animal> getAnimals() { //copy constructor
-		// TODO
-		//System.out.println(navAnimals.get(0));
+	public List<Animal> getAnimals() { 
 		return navAnimals;
 	}
 
@@ -53,10 +52,6 @@ public final class Maze extends World {
 	 */
 
 	public boolean hasAnimal(Animal a) {
-		// TODO
-		//make sure we understand differences of List vs. ArrayList
-		//maybe differentiate between one list containing the animals on the map, and the other in general so we can reset animals respectively
-			//ask certain questions: are all animals always present? Does this change in basis of how much you add, and then these animals can never be fully removed
 		return navAnimals.contains(a);
 	}
 
@@ -67,13 +62,11 @@ public final class Maze extends World {
 	 *            The animal to add
 	 */
 
-	public void addAnimal(Animal a) {
-		//Same for over here
+	public void addAnimal(Animal a) { //the animal is set to the starting position and is appended to both lists
 		Vector2D start = this.getStart();
 		a.setPosition(start);
-		//endAnimals.add(a);
 		navAnimals.add(a);
-		// TODO
+		endAnimals.add(a);
 	}
 
 	/**
@@ -84,32 +77,17 @@ public final class Maze extends World {
 	 */
 
 	public void removeAnimal(Animal a) {
-		//Need to test if the reference stuff has any impact on these commands
-		//getAnimals().remove(a);
-		//a.setPosition(this.getExit());
 		navAnimals.remove(a);
-		endAnimals.add(a);
-		// TODO
 	}
 
 	@Override
 	public void reset() {
-		//I need to pass an instance of the class as an argument to be able to use the getStart() getter
-		//Can we import any classes we want? We would technically need Vector2D
-		for (Animal creature : navAnimals) {
-			endAnimals.add(creature);
-		}
-		
-		navAnimals = new ArrayList<Animal>();
+		navAnimals = new ArrayList<Animal>(); //resets the navigation list to make way for the new animal instances copied from the "final" or end list
 		
 		for (Animal creature : endAnimals) {
 			Animal newCreature = creature.copy();
 			navAnimals.add(newCreature);
 		}
-		
-		
-		
-		// TODO
 	}
 	
 	
